fa7b82355e24513de90dc5bc6baa3a31b111cea7
Swarna7414 <swarnasaisankar044@gmail.com>
2025-04-19 20:51:17 -0500

    Added Skills and FrameWorks

--- src/Interfaces.tsx
+++ src/Interfaces.tsx
@@ -1,15 +1,118 @@
-export interface LoginDetails{
-    Email:string,
-    Password:string
+import React from "react";
+import { PiDiamondDuotone } from "react-icons/pi";
+import { FaJava } from "react-icons/fa";
+import { AiOutlinePython } from "react-icons/ai";
+import { SiTypescript } from "react-icons/si";
+import { ImHtmlFive2 } from "react-icons/im";
+import { GrMysql } from "react-icons/gr";
+import { FaDiamond } from "react-icons/fa6";
+import { FaGitAlt } from "react-icons/fa";
+import { FaBitbucket } from "react-icons/fa";
+import { FaJenkins } from "react-icons/fa";
+import { DiDocker } from "react-icons/di";
+import { GiRecycle } from "react-icons/gi";
+import { SiPostman } from "react-icons/si";
+import { SiSpringboot } from "react-icons/si";
+import { PiNumberCircleFiveFill } from "react-icons/pi";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssLine } from "react-icons/ri";
+
+
+
+interface ProgramingLanguages{
+    language:string;
+    defaulticon:React.ComponentType<{className?:string}>;
+    hovericon:React.ComponentType<{className?:string}>;
 }
 
-export interface RegisterDetails{
-    FirstName:string,
-    LastName:string,
-    Age:string,
-    Email:string,
-    Address:string,
-    Phone:string,
-    Password:string,
-    RePassword:string
-}
+export const languages:ProgramingLanguages[]=[
+    {
+        language:"Java",
+        defaulticon:PiDiamondDuotone,
+        hovericon:FaJava
+    },
+
+    {
+        language:"Python",
+        defaulticon:PiDiamondDuotone,
+        hovericon:AiOutlinePython
+    },
+
+    {
+        language:"TypeScript",
+        defaulticon:PiDiamondDuotone,
+        hovericon:SiTypescript
+    },
+
+    {
+        language:"MySql",
+        defaulticon:PiDiamondDuotone,
+        hovericon:GrMysql
+    },
+
+    {
+        language:"HTML & CSS",
+        defaulticon:PiDiamondDuotone,
+        hovericon:ImHtmlFive2
+    },
+]
+
+export const Certifications:Array<String>=["Programming Fundamentals","Java Programming",
+    "Building Microservices with Springboot","Mathematics for Machine Learning"]
+
+
+export const Tools:ProgramingLanguages[]=[
+    {
+        language:"Git",
+        defaulticon: FaDiamond,
+        hovericon:FaGitAlt
+    },
+    {
+        language:"BitBucket",
+        defaulticon: FaDiamond,
+        hovericon:FaBitbucket
+    },
+    {
+        language:"Jenkins",
+        defaulticon: FaDiamond,
+        hovericon:FaJenkins
+    },
+    {
+        language:"Docker",
+        defaulticon: FaDiamond,
+        hovericon:DiDocker
+    },
+    {
+        language:"RLM (Release LifeCyle Management)",
+        defaulticon: FaDiamond,
+        hovericon:GiRecycle
+    },
+    {
+        language:"PostMan",
+        defaulticon: FaDiamond,
+        hovericon:SiPostman
+    }
+]
+
+export const FrameWorks:ProgramingLanguages[]=[
+    {
+        language:"SpringBoot",
+        defaulticon:FaDiamond,
+        hovericon:SiSpringboot
+    },
+    {
+        language:"Junit & Mockito",
+        defaulticon:FaDiamond,
+        hovericon:PiNumberCircleFiveFill
+    },
+    {
+        language:"React (ts)",
+        defaulticon:FaDiamond,
+        hovericon:FaReact
+    },
+    {
+        language:"Tailwind CSS",
+        defaulticon:FaDiamond,
+        hovericon:RiTailwindCssLine
+    }
+]
--- src/Routw/SandT.tsx
+++ src/Routw/SandT.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { FrameWorks, languages } from "../Interfaces";
 
 
 const SandT:React.FC=()=>{
+  const [hoveredindex,sethoveredindex]=useState<number | null>(null);
+  const [hoveredFrameworkIndex, setHoveredFrameworkIndex] = useState<number | null>(null);
+
+  const renderitem = (
+    item: (typeof languages)[number],
+    index: number
+  ) => {
+    const isHovered = hoveredindex === index;
+    const Icon = isHovered ? item.hovericon : item.defaulticon;
+  
+    return (
+      <div
+        key={index}
+        className="flex flex-row mb-2 mt-1.5 cursor-pointer items-center"
+        onMouseEnter={() => sethoveredindex(index)}
+        onMouseLeave={() => sethoveredindex(null)}
+      >
+        <div
+          className={`transition-all duration-700 ease-in-out transform ${
+            isHovered ? "opacity-100 scale-110" : "opacity-80 scale-100"
+          }`}
+        >
+          <Icon className="text-blue-500 mr-3" />
+        </div>
+  
+        <h1 className="text-lg transition-all duration-700 ease-in-out">
+          {item.language}
+        </h1>
+      </div>
+    );
+  };
+
+
+  const RenderFramework = (
+    item: (typeof FrameWorks)[number],
+    index: number
+  ) => {
+    const isHovered = hoveredFrameworkIndex === index;
+    const Icon = isHovered ? item.hovericon : item.defaulticon;
+  
+    return (
+      <div
+        key={index}
+        className="flex flex-row mb-2 mt-3 cursor-pointer items-center"
+        onMouseEnter={() => setHoveredFrameworkIndex(index)}
+        onMouseLeave={() => setHoveredFrameworkIndex(null)}
+      >
+        <div
+          className={`transition-all duration-700 ease-in-out transform ${
+            isHovered ? "opacity-100 scale-110" : "opacity-80 scale-100"
+          }`}
+        >
+          <Icon className="text-blue-500 mr-3" />
+        </div>
+
+        <h1 className="text-lg transition-all duration-700 ease-in-out">
+          {item.language}
+        </h1>
+      </div>
+    );
+  };
+
   return(
     <section className="bg-black min-h-screen text-blue-100 px-4 py-10">
       <div className="flex justify-center items-center mt-12">
@@ -10,13 +73,23 @@ const SandT:React.FC=()=>{
         </h1>
       </div>
       <div className="flex flex-row gap-25 py-8 px-25">
-        <div className="border-2 border-dashed border-green-100 h-122 w-150 flex flex-col px-5 py-2.5">
-          <h1 className="font-semibold text-2xl mt-2 ml-2">Languages</h1>
-          <div className="mt-1.5 border-1 w-full h-50 rounded-2xl py-4 px-7">
-            
 
+        <div className="border-2 border-dashed border-green-100 h-122 w-150 flex flex-col px-5 pt-2.5">
+          <h1 className="font-semibold text-2xl mt-2 ml-2">Languages</h1>
+          <div className="mt-1.5 border-1 w-full h-50 rounded-2xl py-1.5 px-7">
+            {languages.map((item,index)=> renderitem(item,index))}
+          </div>
+
+          <h1 className="mt-2 font-semibold text-2xl ml-52">FrameWorks</h1>
+          <div className="mt-1.5 border-1 w-89 h-50 rounded-2xl py-1.5 px-7 ml-50.5">
+            {FrameWorks.map((item,index)=>RenderFramework(item,index))}
           </div>
         </div>
+
+
+
+
+
         <div className="border-2 border-dashed border-green-100 h-122 w-150"></div>
       </div>
     </section>
